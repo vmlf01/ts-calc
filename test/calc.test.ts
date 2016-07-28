1a394cba22d36652b1ca240262e11e255b5495da
Vitor Fernandes <vitor.fernandes@gotvmediasoftware.com>
2016-07-28 17:38:10 +0100

    replace chai with should for test assertions

--- test/calc.test.ts
+++ test/calc.test.ts
@@ -1,5 +1,4 @@
-import * as chai from 'chai';
-chai.should();
+import * as should from 'should';
 
 import Calc from '../src/calc';
 
@@ -7,22 +6,23 @@ describe('Calc', () => {
     describe('isEvent', () => {
         it('should return true for 0', () => {
             const result = Calc.isEven(0);
-            result.should.be.true;
+            should.exist(result);
+            result.should.be.true();
         });
 
         it('should return false for 1', () => {
-            Calc.isEven(1).should.be.false;
+            Calc.isEven(1).should.be.false();
         });
 
         it('should return true for 2', () => {
-            Calc.isEven(2).should.be.true;
+            Calc.isEven(2).should.be.true();
         });
 
         it('should return false for -3', () => {
-            Calc.isEven(-3).should.be.false;
+            Calc.isEven(-3).should.be.false();
         });
         it('should return true for -4', () => {
-            Calc.isEven(-4).should.be.true;
+            Calc.isEven(-4).should.be.true();
         });
     });
 });
